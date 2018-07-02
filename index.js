276caef7fd5c83f9fcec55453ea5d47df45cad0f
iasgamer <39603879+iasgamer@users.noreply.github.com>
2018-07-02 16:26:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,104 @@ fs.readdir('./commandes/', (err, files) => {
 
 })
 
+bot.on("guildMemberAdd", member => {
+    var join_embed = new Discord.RichEmbed()
+    .setAuthor("Spyer", bot.user.avatarURL)
+    .setColor("96CA2D")
+    .setFooter("Spyer | Développé par LePtitMetalleux", bot.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTimestamp()
+    .setTitle(`Arrivée d'un nouveau membre !`)
+    .setDescription(`**${member.username}** vient d'arriver sur ${member.guild.name}
+Nombre de membres après l'arrivée de ${member.user.tag} : ${member.guild.memberCount}`)
+    member.guild.channels.find("name", "spyer-logs").send(join_embed)
+    bot.user.setActivity(`Gérer le serveur | Aide ${bot.users.size} utilisateurs`, {type: `PLAYING`});
+});
+
+bot.on("guildMemberRemove", member => {
+    var leave_embed = new Discord.RichEmbed()
+    .setAuthor("Spyer", bot.user.avatarURL)
+    .setColor("B9121B")
+    .setFooter("Spyer | Développé par LePtitMetalleux", bot.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTimestamp()
+    .setTitle(`Départ d'un membre`)
+    .setDescription(`**${member.username}** vient de partir de ${member.guild.name}
+Nombre de membres après le départ de ${member.user.tag} : ${member.guild.memberCount}`)
+    member.guild.channels.find("name", "spyer-logs").send(leave_embed);
+    bot.user.setActivity(`Gérer le serveur | Aide ${bot.users.size} utilisateurs`, {type: `PLAYING`});
+    });
+
+bot.on("channelCreate", channel => {
+    var channel_create_embed = new Discord.RichEmbed()
+    .setAuthor("Spyer", bot.user.avatarURL)
+    .setColor("96CA2D")
+    .setFooter("Spyer | Développé par LePtitMetalleux", bot.user.avatarURL)
+    .setTimestamp()
+    .setTitle("Nouveau salon créé !")
+    .setDescription(`Nom du salon : ${channel.name}
+Nombre de salons après l'ajout du salon : ${channel.guild.channels.size}`)
+    channel.guild.channels.find("name", "spyer-logs").send(channel_create_embed);
+  });
+
+bot.on("channelDelete", channel => {
+    var channel_delete_embed = new Discord.RichEmbed()
+    .setAuthor("Spyer", bot.user.avatarURL)
+    .setColor("B9121B")
+    .setFooter("Spyer | Développé par LePtitMetalleux", bot.user.avatarURL)
+    .setTimestamp()
+    .setTitle("Salon supprimé")
+    .setDescription(`Nom du salon : ${channel.name}
+Nombre de salons après la suppression du salon : ${channel.guild.channels.size}`)
+    channel.guild.channels.find("name", "spyer-logs").send(channel_delete_embed);
+    });
+
+bot.on("roleCreate", role => {
+    const rolec_embed = new Discord.RichEmbed()
+    .setAuthor("Spyer", bot.user.avatarURL)
+    .setColor("96CA2D")
+    .setFooter("Spyer | Développé par LePtitMetalleux", bot.user.avatarURL)
+    .setTimestamp()
+    .setTitle("Un rôle a été crée !")
+    .setDescription(`Nom du rôle : ${role.name}
+Nombre de rôles après l'ajout du rôle ${role.name} : ${role.guild.roles.size}`)
+    role.guild.channels.find("name", "spyer-logs").send(rolec_embed);
+    });
+
+bot.on("roleDelete", role => {
+    var roled_embed = new Discord.RichEmbed()
+    .setColor("B9121B")
+    .setAuthor(bot.user.tag, bot.user.avatarURL)
+    .setTitle("Un rôle a été supprimé")
+    .setDescription(`Nom du rôle : ${role.name}
+Nombre de rôles après la suppression du rôle ${role.name} : ${role.guild.roles.size}`)
+    role.guild.channels.find("name", "spyer-logs").send(roled_embed);
+    });
+
+bot.on("messageUpdate", (oldMessage, newMessage) => {
+    if(oldMessage.author.bot || oldMessage.cleanContent === newMessage.cleanContent) return;
+    let msgup_embed = new Discord.RichEmbed()
+    .setAuthor(newMessage.member.user.tag, newMessage.member.user.avatarURL)
+    .setColor("FF5B2B")
+    .setTitle(`Un message a été modifié !`)
+    .setDescription(`Le message de ${newMessage.author.tag} a été modifié.
+Ancien message : ``${oldMessage.cleanContent}``
+Nouveau message : ``${newMessage.cleanContent}```)
+    newMessage.guild.channels.find("name", "spyer-logs").send(msgup_embed);
+    });
+
+bot.on("messageDelete", (message) => {
+    if (message.author.bot) return;
+    let msgdel_embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag, message.author.avatarURL)
+    .setColor("B9121B")
+    .setTitle("Un message a été supprimé !")
+    .setDescription(`Le message de ${message.author.tag} a été supprimé.
+Message Supprimé : `+``+ `${message.cleanContent}`+``)
+    message.guild.channels.find("name", "spyer-logs").send(msgdel_embed);
+  });
+  
+
 bot.on('message', message => {
 
     var cont = message.content.slice(prefix.length).split(" ");
@@ -37,7 +135,9 @@ bot.on("ready", async () => {
     });
 
 bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "💬-général").send(`${member} a rejoint le serveur ! Bienvenue à toi !`)
+    member.guild.channels.find("name", "💬-général").send(`${member} a rejoint le serveur ! Bienvenue à toi !
+Un giveway sera organisé lors du passage des 100 membres ! 
+(${bot.users.size}/100 soit ${100 - bot.users.size} membres avant le prochain giveway)`)
     var role1 = member.guild.roles.find('name', '📢Notif-Bot');
     var role2 = member.guild.roles.find('name', '📢Notif-Serveur');
     var role3 = member.guild.roles.find('name', '📢Notif-Event');
